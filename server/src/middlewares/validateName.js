b4ad00cb15a3cb5639c6ad792b5596c4240473ad
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-22 17:33:23 -0300

    Feat: Cria sistema para validação do name

---
+++ server/src/middlewares/validateName.js
@@ -0,0 +1,14 @@
+const validateName = (req, res, next) => {
+    const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).json({ message: 'O campo "nome" é obrigatório' });
+    }
+    if (username.length < 8) {
+        return res.status(400).json({ message: 'O "nome" deve ter pelo menos 4 caracteres' });
+    }
+
+    return next();
+};
+
+module.exports = validateName;
