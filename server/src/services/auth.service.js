bb57c5156f3d93218a21a7d72a0fdd31262a14c2
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-22 18:37:02 -0300

    Feat: Cria função para formatar a data e salvar no banco

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -14,6 +14,16 @@ const verifyIfExistUser = async (email) => {
     return { user };
 };
 
+function formatDateBrazilian(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear());
+
+    return `${day}/${month}/${year}`;
+}
+
+// Obtendo a data atual e formatando para o formato brasileiro:
+
 const createUser = async (userData, invitedBy) => {
     const {
         username, email, tell, isAdmin,
@@ -31,6 +41,7 @@ const createUser = async (userData, invitedBy) => {
         tell,
         invitedBy,
         isAdmin,
+        createdAt: formatDateBrazilian(new Date()),
     });
 
     newUser.password = undefined;
