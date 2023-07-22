1a34e411d614fde98b0b3f37be8a0c476c2c44f0
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-22 17:11:15 -0300

    Feat: Cria validação para ver se o usuário existe e se é um admin

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -3,10 +3,34 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 const User = require('../models/user.model');
 
+const verifyIfExistUser = async (email) => {
+    const user = await User.findOne({ email });
+    if (user) {
+        return { message: 'Usuário já existe' };
+    }
+    return { user };
+};
+
+const verifyIfUserIsAdmin = async (id) => {
+    const admin = await User.findById({ _id: id });
+
+    if (admin && !admin.isAdmin) {
+        return { message: 'Usuário não é admin' };
+    }
+
+    return { admin };
+};
+
 const createUser = async (userData) => {
     const {
         username, password, email, tell, invitedBy, isAdmin,
     } = userData;
+
+    const adminExist = await verifyIfUserIsAdmin(invitedBy);
+    if (adminExist.message) return adminExist;
+    const userExist = await verifyIfExistUser(email);
+    if (userExist.message) return userExist;
+
     const hash = password ? await bcrypt.hash(password, 10) : await bcrypt.hash('123456', 10);
     const newUser = await User.create({
         username,
