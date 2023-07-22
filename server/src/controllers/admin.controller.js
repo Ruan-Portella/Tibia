84b27eaefc1c09053008d4bd18259d7a59aa4522
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-22 18:38:35 -0300

    Feat: Cria função na service para pegar todos usuários no controller

---
+++ server/src/controllers/admin.controller.js
@@ -0,0 +1,14 @@
+const adminService = require('../services/admin.service');
+
+const getAllUsers = async (_req, res) => {
+    try {
+        const users = await adminService.getAllUsers();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    getAllUsers,
+};
