5fcc671c0b8c1a7eeb4a2ee085ceba31e83dde25
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-22 17:28:27 -0300

    Feat: Cria rota para login

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/signup', authController.createUser);
+router.post('/login', authController.login);
 
 module.exports = router;
