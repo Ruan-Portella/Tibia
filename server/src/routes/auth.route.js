e6363ce2d14be4dedce00c8c45c7a984f14d390f
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-22 18:37:30 -0300

    Feat: Retira o validate token da rota do login

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -5,6 +5,6 @@ const validations = require('../middlewares');
 const router = express.Router();
 
 router.post('/createUser', validations.validateEmail, validations.validatePassword, validations.validateName, validations.validateToken, validations.validateAdmin, authController.createUser);
-router.post('/login', validations.validateEmail, validations.validatePassword, validations.validateName, validations.validateToken, authController.login);
+router.post('/login', validations.validateEmail, validations.validatePassword, validations.validateName, authController.login);
 
 module.exports = router;
