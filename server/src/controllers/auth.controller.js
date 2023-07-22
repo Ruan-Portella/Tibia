3374dc39b16a4979de6cb15c66ee9ffd496381b5
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-22 17:28:17 -0300

    Feat: Cria função para login no controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,8 +2,17 @@ const authService = require('../services/auth.service');
 
 const createUser = async (req, res) => {
     try {
-        const user = await authService.createUser(req.body);
+        const user = await authService.createUser(req.body, req.user.id);
         res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const token = await authService.login(req.body);
+        res.status(200).json(token);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -11,4 +20,5 @@ const createUser = async (req, res) => {
 
 module.exports = {
     createUser,
+    login,
 };
