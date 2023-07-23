b4dd1ec1063bec3da64f99b3b6ebfd186a130b68
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 13:05:58 -0300

    Feat: Cria rota para fazer o post

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -4,6 +4,7 @@ const validations = require('../middlewares');
 
 const router = express.Router();
 
-router.get('/:id', validations.validateToken, userController.getUserById);
+router.get('/:id', validations.validateToken, validations.validateAdmin, userController.getUserById);
+router.post('/:id', validations.validateToken, userController.createChar);
 
 module.exports = router;
