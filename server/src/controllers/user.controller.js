e2537a9fdcb6b485d673ac2912e8191fd1fc4c43
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 13:05:49 -0300

    Feat: Cria função para criar char

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -12,6 +12,19 @@ const getUserById = async (req, res) => {
     }
 };
 
+const createChar = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { id: userId } = req.user;
+        if (userId !== id) throw new Error('Você não tem permissão para criar um personagem para este usuário');
+        const char = await userService.createChar(req.body, id);
+        res.status(200).json(char);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
     getUserById,
+    createChar,
 };
