dac19c8ee095da552979004a3fee0d3bcaa11fd0
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 12:28:27 -0300

    Feat: Passa a função de criar um usuário para a rota admin e cria função para deletar

--- server/src/routes/admin.route.js
+++ server/src/routes/admin.route.js
@@ -5,5 +5,7 @@ const validations = require('../middlewares');
 const router = express.Router();
 
 router.get('/', validations.validateToken, validations.validateAdmin, adminController.getAllUsers);
+router.post('/', validations.validateEmail, validations.validatePassword, validations.validateName, validations.validateToken, validations.validateAdmin, adminController.createUser);
+router.delete('/', validations.validateToken, validations.validateAdmin, adminController.deleteUser);
 
 module.exports = router;
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -4,7 +4,6 @@ const validations = require('../middlewares');
 
 const router = express.Router();
 
-router.post('/createUser', validations.validateEmail, validations.validatePassword, validations.validateName, validations.validateToken, validations.validateAdmin, authController.createUser);
 router.post('/login', validations.validateEmail, validations.validatePassword, validations.validateName, authController.login);
 
 module.exports = router;
