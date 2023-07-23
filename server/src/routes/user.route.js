6469cb791cf916f34b5f276bcdb69b0fb2d1eab7
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 13:33:32 -0300

    Feat: Cria a rota put para profile

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -5,6 +5,7 @@ const validations = require('../middlewares');
 const router = express.Router();
 
 router.get('/:id', validations.validateToken, validations.validateAdmin, userController.getUserById);
+router.put('/:id/profile', validations.validateToken, userController.updateUser);
 router.post('/:id', validations.validateToken, userController.createChar);
 router.put('/:id', validations.validateToken, userController.updateChar);
 router.delete('/:id', validations.validateToken, userController.deleteChar);
