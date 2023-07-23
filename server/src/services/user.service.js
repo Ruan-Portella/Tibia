00e06c702e9ffaf65e1b358a58ea79cbe4dce317
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 13:05:37 -0300

    Feat: Cria função para criar char e melhora a função de getUserById

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -1,14 +1,25 @@
 /* eslint-disable no-underscore-dangle */
 require('dotenv').config();
 const User = require('../models/user.model');
-// const authService = require('./auth.service');
+const Char = require('../models/char.model');
 
 const getUserById = async (id) => {
     const user = await User.findById(id);
     if (!user) throw new Error('Usuário não encontrado');
+    const chars = await Char.find({ userId: id });
+    user.chars = chars;
+    user.password = undefined;
+    user.email = undefined;
     return user;
+};
+
+const createChar = async (char, userId) => {
+    await getUserById(userId);
+    const newChar = Char.create({ userId, ...char });
+    return newChar;
 };
 
 module.exports = {
     getUserById,
+    createChar,
 };
