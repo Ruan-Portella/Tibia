234b0bda198226455e64fc88199ad85649ac05f4
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 12:32:44 -0300

    Feat: Cria rota put

--- server/src/routes/admin.route.js
+++ server/src/routes/admin.route.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', validations.validateToken, validations.validateAdmin, adminController.getAllUsers);
 router.post('/', validations.validateEmail, validations.validatePassword, validations.validateName, validations.validateToken, validations.validateAdmin, adminController.createUser);
 router.delete('/', validations.validateToken, validations.validateAdmin, adminController.deleteUser);
+router.put('/', validations.validateToken, validations.validateAdmin, adminController.updateUser);
 
 module.exports = router;
