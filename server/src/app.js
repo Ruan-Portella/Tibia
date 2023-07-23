22154f25bd0da51d45b6be0948764b8e43736378
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 12:39:35 -0300

    Feat: Utiliza rota user no app

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRouter = require('./routes/auth.route');
 const adminRouter = require('./routes/admin.route');
+const userRouter = require('./routes/user.route');
 
 require('dotenv').config();
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/user', userRouter);
 
 module.exports = app;
