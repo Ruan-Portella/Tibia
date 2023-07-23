2dbe0b114df5fb724727365bdaac5c1f5b618ba0
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-23 12:28:15 -0300

    Feat: Passa a função de criar um usuário para a rota admin

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -9,6 +9,27 @@ const getAllUsers = async (_req, res) => {
     }
 };
 
+const createUser = async (req, res) => {
+    try {
+        const user = await adminService.createUser(req.body, req.user.id);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const { userId: id } = req.body;
+        const user = await adminService.deleteUser(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
     getAllUsers,
+    createUser,
+    deleteUser,
 };
