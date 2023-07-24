0de33c63db23efdd77217eab65608b96ddb4418d
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-24 17:33:28 -0300

    Feat: Cria página para users

---
+++ client/src/app/user/[userId]/page.jsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function User() {
+    return (
+        <main>
+            <Header />
+            {/* <h1>User</h1> */}
+        </main>
+    )
+}
